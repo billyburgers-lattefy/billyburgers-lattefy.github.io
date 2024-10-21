acd1c31f087272234d1aa8a1253c3502f031f975
Billy Burgers - Lattefy <billyburgers.lattefy@gmail.com>
2024-10-20 22:46:59 -0300

    Add files via upload

--- fidelity/assets/script.js
+++ fidelity/assets/script.js
@@ -1,6 +1,15 @@
 // BILLY BURGERS - SCRIPT
 
 
+// Hide toolbar
+
+window.addEventListener("load",function() {
+    setTimeout(function(){
+        // This hides the address bar:
+        window.scrollTo(0, 1)
+    }, 0)
+})
+
 // NavBar - toggle Responsive Menu:
 
 burger = document.querySelector(".burger")
