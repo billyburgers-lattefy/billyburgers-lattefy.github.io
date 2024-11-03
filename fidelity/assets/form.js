f0f9d85104fcd6f4533bc9cf4a1b85ca910fedac
Billy Burgers - Lattefy <billyburgers.lattefy@gmail.com>
2024-11-03 14:00:38 -0300

    update form.js

--- fidelity/assets/form.js
+++ fidelity/assets/form.js
@@ -1,7 +1,7 @@
 // Billy Burgers - Fidelity Card | Form
 
-// const apiUrl = 'https://backend-nqez.onrender.com';
-const apiUrl = 'http://localhost:3068';
+const apiUrl = 'https://backend-nqez.onrender.com';
+// const apiUrl = 'http://localhost:3068';
 
 // Fetch client by phone number
 async function getClientByPhoneNumber(phoneNumber) {
