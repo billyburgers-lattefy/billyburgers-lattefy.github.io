066fd55745298c2405ef2a854ac921c459192ab3
Billy Burgers - Lattefy <billyburgers.lattefy@gmail.com>
2024-11-08 21:36:11 -0300

    Add files via upload

--- dashboard/assets/script.js
+++ dashboard/assets/script.js
@@ -3,15 +3,15 @@
 // Sidebar
 
 document.addEventListener('DOMContentLoaded', function () {
-  const sidebarBtn = document.getElementById('sidebar-btn')
-  const sidebar = document.querySelector('.sidebar')
-  const mainContent = document.querySelector('.main')
-  const navbar = document.querySelector('.navbar')
-
-  sidebarBtn.addEventListener('click', function () {
-      sidebar.classList.toggle('active')
-      document.body.classList.toggle('collapsed-sidebar')
-      mainContent.classList.toggle('full-width')
-      navbar.classList.toggle('full-width')
+    const sidebarBtn = document.getElementById('sidebar-btn')
+    const sidebar = document.querySelector('.sidebar')
+    const mainContent = document.querySelector('.main')
+    const navbar = document.querySelector('.navbar')
+  
+    sidebarBtn.addEventListener('click', function () {
+        sidebar.classList.toggle('active')
+        document.body.classList.toggle('collapsed-sidebar')
+        mainContent.classList.toggle('full-width')
+        navbar.classList.toggle('full-width')
+    })
   })
-})
